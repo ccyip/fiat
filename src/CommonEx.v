a2a16440dfbeb1f79510231a9d1de0302a6ab761
Qianchuan Ye <yeqianchuan@gmail.com>
2018-05-17 22:42:44 -0400

    Add function composition combination and a weakening lemma.

--- src/CommonEx.v
+++ src/CommonEx.v
@@ -1,9 +1,11 @@
 Require Import
         FunctionalExtensionality
+        Coq.Sets.Image
         Coq.omega.Omega
         Coq.Logic.Eqdep_dec.
 
 Require Import
+        Fiat.Common
         Fiat.Computation
         Fiat.Computation.Core
         Fiat.Narcissus.Common.Specs
@@ -70,3 +72,130 @@ Proof.
   rewrite <- pf.
   exact v.
 Defined.
+
+Definition type_cast_r {A B : Type} (pf : B = A) (v : A) : B.
+Proof.
+  rewrite pf.
+  exact v.
+Defined.
+
+Import Ensembles.
+
+Lemma fun_compose_format_correct
+      {A A' B} {cache : Cache} {monoid : Monoid B}
+      {P : CacheDecode -> Prop}
+      {P_inv : (CacheDecode -> Prop) -> Prop}
+      (f : A -> A') (f_inv : A' -> A)
+      (im : A' -> bool)
+      (predicate : A -> Prop)
+      (predicate' : A' -> Prop)
+      (predicate_rest : A -> B -> Prop)
+      (predicate_rest' : A' -> B -> Prop)
+      (format : FormatM A' B)
+      (decode : DecodeM A' B)
+      (f_OK : forall a, predicate a -> f_inv (f a) = a)
+      (im_OK : forall a', decides (im a') (In _ (Im _ _ predicate f) a'))
+      (P_inv_OK : cache_inv_Property P P_inv)
+      (decode_correct :
+         cache_inv_Property P P_inv
+         -> CorrectDecoder
+             monoid predicate'
+             predicate_rest'
+             format decode P)
+      (predicate_OK : forall a,
+          predicate a ->
+          predicate' (f a))
+      (predicate_rest_OK : forall a b,
+          predicate a ->
+          predicate_rest a b ->
+          predicate_rest' (f a) b)
+  : CorrectDecoder
+      monoid
+      predicate
+      predicate_rest
+      (fun a ce => format (f a) ce)
+      (fun b cd => `(a', b', cd') <- decode b cd;
+                  if im a' then Some (f_inv a', b', cd') else None)
+      P.
+Proof.
+  split; intros. {
+    destruct decode_correct as [[? [? [? ?]]] _]; eauto.
+    eexists. repeat split; eauto.
+    rewrite H3. simpl.
+    specialize (im_OK (f data)).
+    destruct im. repeat progress f_equal. auto.
+    simpl in im_OK. exfalso. auto with sets.
+  } {
+    decode_opt_to_inv.
+    destruct decode_correct as [_ [? [? [? [? [? [? ?]]]]]]]; eauto.
+    specialize (im_OK x).
+    destruct im; try easy. simpl in im_OK.
+    injections.
+    apply Im_inv in im_OK. destruct im_OK as [? [? ?]].
+    specialize (f_OK _ H2). subst. rewrite f_OK.
+    split; eauto. eexists _, _. repeat split; eauto.
+  }
+Qed.
+
+Lemma shrink_format_correct
+      {A B} {cache : Cache} {monoid : Monoid B}
+      {P : CacheDecode -> Prop}
+      {P_inv : (CacheDecode -> Prop) -> Prop}
+      (predicate : A -> Prop) (predicate_rest : A -> B -> Prop)
+      (predicate' : A -> Prop) (predicate_rest' : A -> B -> Prop)
+      (format : FormatM A B)
+      (decode : DecodeM A B)
+      (predicate_dec : A -> bool)
+      (predicate_dec_OK : forall a, decides (predicate_dec a) (predicate a))
+      (P_inv_OK : cache_inv_Property P P_inv)
+      (decode_correct :
+         cache_inv_Property P P_inv ->
+         CorrectDecoder monoid predicate' predicate_rest' format decode P)
+      (predicate_OK : forall a, predicate a -> predicate' a)
+      (predicate_rest_OK : forall a b, predicate a -> predicate_rest a b -> predicate_rest' a b)
+  : CorrectDecoder
+      monoid predicate predicate_rest
+      format
+      (fun b cd => `(a, b', cd') <- decode b cd;
+                  if predicate_dec a then Some (a, b', cd') else None)
+      P.
+Proof.
+  split; intros. {
+    specialize (predicate_dec_OK data).
+    destruct decode_correct as [[? [? [? ?]]] _]; eauto.
+    eexists. rewrite H3. simpl. destruct predicate_dec; eauto.
+    congruence.
+  } {
+    decode_opt_to_inv.
+    specialize (predicate_dec_OK x). destruct predicate_dec; try easy.
+    injections.
+    destruct decode_correct as [_ [? [? [? [? [? [? ?]]]]]]]; eauto.
+    split; auto. eexists _, _. repeat split; eauto.
+  }
+Qed.
+
+Lemma shrink_format_correct_True
+      {A B} {cache : Cache} {monoid : Monoid B}
+      {P : CacheDecode -> Prop}
+      {P_inv : (CacheDecode -> Prop) -> Prop}
+      (predicate : A -> Prop) (predicate_rest : A -> B -> Prop)
+      (predicate' : A -> Prop) (predicate_rest' : A -> B -> Prop)
+      (format : FormatM A B)
+      (decode : DecodeM A B)
+      (P_inv_OK : cache_inv_Property P P_inv)
+      (decode_correct :
+         cache_inv_Property P P_inv ->
+         CorrectDecoder monoid predicate' predicate_rest' format decode P)
+      (predicate_OK : forall a, predicate a)
+      (predicate_OK' : forall a, predicate' a)
+      (predicate_rest_OK : forall a b, predicate_rest a b -> predicate_rest' a b)
+  : CorrectDecoder
+      monoid predicate predicate_rest format decode P.
+Proof.
+  split; intros. {
+    destruct decode_correct as [[? [? [? ?]]] _]; eauto.
+  } {
+    destruct decode_correct as [_ [? [? [? [? [? [? ?]]]]]]]; eauto.
+    split; auto. eexists _, _. repeat split; eauto.
+  }
+Qed.
