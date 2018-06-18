f73836b94bf4a5a0173c95a5fff35c2dc0060fef
Qianchuan Ye <yeqianchuan@gmail.com>
2018-06-18 00:39:40 -0400

    Fix LengthDelimited decoder.

--- src/Narcissus/Examples/ProtobufLengthDelimited.v
+++ src/Narcissus/Examples/ProtobufLengthDelimited.v
@@ -18,8 +18,6 @@ Require Import
         Fiat.Narcissus.BinLib.Core
         Fiat.Narcissus.BinLib.AlignedByteString
         Fiat.Narcissus.BinLib.AlignWord
-        Fiat.Narcissus.BinLib.AlignedList
-        Fiat.Narcissus.BinLib.AlignedDecoders
         Fiat.Narcissus.Formats.Option
         Fiat.Narcissus.Formats.FixListOpt
         Fiat.Narcissus.Formats.SizedListOpt
@@ -52,13 +50,9 @@ Section LengthDelimited.
                                                          A_format x ce2 ↝ (b2, ce2') ->
                                                          bin_measure b1 = bin_measure b2.
   Variable A_format_byte : forall d b ce ce', A_format d ce ↝ (b, ce') -> bin_measure b mod 8 = 0.
+  Variable A_format_some : forall d b ce ce', A_format d ce ↝ (b, ce') -> (0 < bin_measure b)%nat.
   Variable A_decode_lt : forall b cd x b' cd', A_decode b cd = Some (x, b', cd') -> lt_B b' b.
   Variable A_decode_correct : CorrectDecoder monoid A_predicate A_predicate_rest A_format A_decode A_cache_inv.
-
-  Variable Wf_bound : B.
-  Variable Wf_decode : forall b, lt_B b Wf_bound -> CacheDecode -> option (A * B * CacheDecode).
-  Variable Wf_decode_lt : forall b pf cd x b' cd', Wf_decode b pf cd = Some (x, b', cd') -> lt_B b' b.
-  Variable Wf_decode_correct : CorrectDecoderWf monoid A_predicate A_predicate_rest A_format Wf_decode A_cache_inv.
 
   Definition PB_LengthDelimited_format
     : FormatM (list A) B :=
@@ -72,22 +66,10 @@ Section LengthDelimited.
     fun b cd =>
       `(sz, b1, cd1) <- (`(x, b1, cd1) <- Varint_decode b cd;
                           Some (N.to_nat x, b1, cd1));
-        SizedList_decode A_decode A_decode_lt
-                         (sz * 8) b1 cd.
-
-  Definition PB_LengthDelimitedWf_decode
-    : forall b, lt_B b Wf_bound -> CacheDecode -> option (list A * B * CacheDecode).
-  Proof.
-    refine
-      (fun b pf cd =>
-         `(sz, b1, cd1) <- (`(x, b1, cd1) <- Decode_w_Measure_lt Varint_decode b cd Varint_decode_lt;
-                           Some (N.to_nat x, b1, cd1));
-         SizedListWf_decode Wf_bound Wf_decode Wf_decode_lt
-                            (sz * 8) (proj1_sig b1) _ cd).
-    abstract (destruct b1; simpl; unfold lt_B in *; omega).
-  Defined.
+        SizedList_decode A_decode (sz * 8) b1 cd.
 
   Local Arguments Nat.div : simpl never.
+  Local Opaque Varint_decode.
   Theorem PB_LengthDelimited_decode_correct
           (A_cache_inv_OK : cache_inv_Property A_cache_inv (fun P => forall b cd, P cd -> P (addD cd b)))
     : CorrectDecoder monoid
@@ -104,7 +86,7 @@ Section LengthDelimited.
         in Hv.
       edestruct Hv as [[? [? [? ?]]] _]; eauto. clear H4 H5.
       edestruct (SizedList_decode_correct (A:=A)) as [[? [? [? ?]]] _]; try apply H2; eauto.
-      intuition. eapply SizedList_format_sz_eq; eauto.
+      unfold SizedList_predicate. intuition. eapply SizedList_format_sz_eq; eauto.
       eexists. repeat split; eauto.
       rewrite <- mappend_assoc. rewrite H3.
       simpl. rewrite Nat.mul_comm.
@@ -122,7 +104,7 @@ Section LengthDelimited.
       eapply fun_compose_format_correct
         with (predicate:=fun _ => True) (predicate_rest:=fun _ _ => True) (im:=fun _ => true)
         in Hv.
-      edestruct Hv as [_ [? [? [? [? [? [? ?]]]]]]]; eauto.
+      edestruct (Hv bin) as [_ [? [? [? [? [? [? ?]]]]]]]; eauto.
       rewrite H1. simpl. reflexivity.
       edestruct (SizedList_decode_correct (A:=A)) as [_ [? [? [? [? [? [[? ?] ?]]]]]]]; try apply H2; eauto.
       split; eauto.
@@ -135,55 +117,6 @@ Section LengthDelimited.
       intros. apply Nnat.Nat2N.id.
       intros. simpl. econstructor. intuition. symmetry. apply Nnat.N2Nat.id.
     }
-  Qed.
-
-  Theorem PB_LengthDelimitedWf_decode_correct
-          (A_cache_inv_OK : cache_inv_Property A_cache_inv (fun P => forall b cd, P cd -> P (addD cd b)))
-    : CorrectDecoderWf monoid
-                     (fun xs => forall x, In x xs -> A_predicate x)
-                     (SizedList_predicate_rest A_predicate_rest A_format)
-                     PB_LengthDelimited_format PB_LengthDelimitedWf_decode A_cache_inv.
-  Proof.
-    unfold PB_LengthDelimited_format, PB_LengthDelimitedWf_decode.
-    split; intros. {
-      computes_to_inv2.
-      assert (lt_B (mappend b0 ext) Wf_bound) as pf'. {
-        unfold lt_B in *. rewrite !mappend_measure in pf. rewrite mappend_measure. omega.
-      }
-      pose proof (Varint_decode_correct (P:=A_cache_inv)) as Hv.
-      edestruct Hv as [[? [? [? ?]]] _]; eauto. clear H4 H5.
-      edestruct (SizedListWf_decode_correct (A:=A)) as [[? [? [? ?]]] _]; try apply H2; eauto.
-      intuition. eapply SizedList_format_sz_eq; eauto.
-      eexists. repeat split; eauto.
-      edestruct @Decode_w_Measure_lt_eq with (A_decode_lt:=Varint_decode_lt); eauto.
-      revert pf. rewrite <- mappend_assoc. intros. rewrite H7.
-      simpl. rewrite Nat.mul_comm.
-      assert (bin_measure b0 mod 8 = 0) as L. {
-        eapply SizedList_format_byte; eauto.
-      }
-      apply Nat.div_exact in L; eauto. rewrite Nnat.Nat2N.id.
-      rewrite <- L; eauto.
-      match goal with
-      | H : SizedListWf_decode _ _ _ _ _ ?p _ = _ |- 
-        SizedListWf_decode _ _ _ _ _ ?p' _ = _ =>
-        replace p' with p
-      end. apply H4.
-      apply le_uniqueness_proof.
-    } {
-      decode_opt_to_inv.
-      subst. apply Decode_w_Measure_lt_eq_inv in H1. destruct x3. simpl in H1.
-      pose proof (Varint_decode_correct (P:=A_cache_inv)) as Hv.
-      edestruct Hv as [_ [? [? [? [? [? [? ?]]]]]]]; eauto.
-      edestruct (SizedListWf_decode_correct (A:=A)) as [_ [? [? [? [? [? [[? ?] ?]]]]]]]; try apply H2; eauto.
-      split; eauto.
-      eexists _, _. repeat split; eauto.
-      computes_to_econstructor; eauto.
-      computes_to_econstructor; eauto.
-      apply H11 in H9. simpl fst. rewrite H9. rewrite Nat.div_mul by auto. rewrite Nnat.N2Nat.id. eauto.
-      simpl fst. rewrite <- mappend_assoc. subst. simpl in *. congruence.
-    }
-    Grab Existential Variables.
-    auto.
   Qed.
 
   Theorem PB_LengthDelimited_format_sz_eq
@@ -208,18 +141,7 @@ Section LengthDelimited.
     unfold PB_LengthDelimited_decode. intros.
     decode_opt_to_inv.
     apply Varint_decode_lt in H.
-    apply SizedList_decode_le in H0.
-    unfold lt_B, le_B in *. subst. omega.
-  Qed.
-
-  Theorem PB_LengthDelimitedWf_decode_lt
-    : forall b pf cd d b' cd',
-      PB_LengthDelimitedWf_decode b pf cd = Some (d, b', cd') -> lt_B b' b.
-  Proof.
-    unfold PB_LengthDelimited_decode. intros.
-    decode_opt_to_inv.
-    apply Decode_w_Measure_lt_eq_inv in H. destruct x3, x0. simpl in *.
-    apply SizedListWf_decode_le in H0.
+    apply SizedList_decode_le in H0; eauto.
     unfold lt_B, le_B in *. subst. omega.
   Qed.
 
