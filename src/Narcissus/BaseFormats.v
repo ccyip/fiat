3a1b7db7facc1eae7e099ac64cfe66f5847c9e88
Qianchuan Ye <yeqianchuan@gmail.com>
2018-11-25 16:53:41 -0500

    Disabled all base formats using old definitions. They will be reenabled later.

--- src/Narcissus/BaseFormats.v
+++ src/Narcissus/BaseFormats.v
@@ -1,8 +1,9 @@
 Require Export
-        Fiat.Narcissus.Formats.Base.SequenceFormat
         Fiat.Narcissus.Formats.Base.FMapFormat
-        Fiat.Narcissus.Formats.Base.StrictTerminalFormat
-        Fiat.Narcissus.Formats.Base.LaxTerminalFormat
-        Fiat.Narcissus.Formats.Base.FixFormat
-        Fiat.Narcissus.Formats.Base.EnqueueFormat
-        Fiat.Narcissus.Formats.Base.UnionFormat.
+        Fiat.Narcissus.Formats.Base.UnionFormat
+        (* Fiat.Narcissus.Formats.Base.SequenceFormat *)
+        (* Fiat.Narcissus.Formats.Base.StrictTerminalFormat *)
+        (* Fiat.Narcissus.Formats.Base.LaxTerminalFormat *)
+        (* Fiat.Narcissus.Formats.Base.FixFormat *)
+        (* Fiat.Narcissus.Formats.Base.EnqueueFormat *)
+        .
