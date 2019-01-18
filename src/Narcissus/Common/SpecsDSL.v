8df93a87174c5f51f8ac5d1c16e5928c81144e70
Qianchuan Ye <yeqianchuan@gmail.com>
2019-01-18 15:06:24 -0500

    Renamed Primitive to Artitrary.

--- src/Narcissus/Common/SpecsDSL.v
+++ src/Narcissus/Common/SpecsDSL.v
@@ -3,7 +3,7 @@ Require Export
         Fiat.Narcissus.BaseFormats.
 
 Inductive SpecsDSL S T : Type :=
-| SL_Primitive: FormatM S T -> SpecsDSL S T
+| SL_Arbitrary: FormatM S T -> SpecsDSL S T
 | SL_Compose {S'}: SpecsDSL S' T -> SpecsDSL S S' -> SpecsDSL S T
 | SL_Projection {S'}: SpecsDSL S' T -> (S -> S') -> SpecsDSL S T
 | SL_Restrict: SpecsDSL S T -> (S -> Prop) -> SpecsDSL S T
@@ -14,7 +14,7 @@ Inductive SpecsDSL S T : Type :=
 
 Fixpoint SpecsDSL_denote {S T} (dsl : SpecsDSL S T) : FormatM S T :=
   match dsl with
-  | SL_Primitive fmt => fmt
+  | SL_Arbitrary fmt => fmt
   | SL_Compose _ fmt1 fmt2 => Compose_Format (SpecsDSL_denote fmt1) (SpecsDSL_denote fmt2)
   | SL_Projection _ fmt1 f => Projection_Format (SpecsDSL_denote fmt1) f
   | SL_Restrict fmt1 P => Restrict_Format P (SpecsDSL_denote fmt1)
