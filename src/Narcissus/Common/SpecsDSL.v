1b632ddcfcbcabbe0696f449ed0d9ba929ee379d
Qianchuan Ye <yeqianchuan@gmail.com>
2019-01-18 14:58:00 -0500

    Ignored Fixpoint combinator for now.

--- src/Narcissus/Common/SpecsDSL.v
+++ src/Narcissus/Common/SpecsDSL.v
@@ -9,7 +9,7 @@ Inductive SpecsDSL S T : Type :=
 | SL_Restrict: SpecsDSL S T -> (S -> Prop) -> SpecsDSL S T
 | SL_Union: SpecsDSL S T -> SpecsDSL S T -> SpecsDSL S T
 | SL_Sequence `{Monoid T}: SpecsDSL S T -> SpecsDSL S T -> SpecsDSL S T
-| SL_Fixpoint: (FormatM S T -> SpecsDSL S T) -> SpecsDSL S T
+(* | SL_Fixpoint: (FormatM S T -> SpecsDSL S T) -> SpecsDSL S T *)
 .
 
 Fixpoint SpecsDSL_denote {S T} (dsl : SpecsDSL S T) : FormatM S T :=
@@ -20,5 +20,5 @@ Fixpoint SpecsDSL_denote {S T} (dsl : SpecsDSL S T) : FormatM S T :=
   | SL_Restrict fmt1 P => Restrict_Format P (SpecsDSL_denote fmt1)
   | SL_Union fmt1 fmt2 => Union_Format (SpecsDSL_denote fmt1) (SpecsDSL_denote fmt2)
   | SL_Sequence _ fmt1 fmt2 => Sequence_Format (SpecsDSL_denote fmt1) (SpecsDSL_denote fmt2)
-  | SL_Fixpoint F => Fix_Format (fun rec => SpecsDSL_denote (F rec))
+  (* | SL_Fixpoint F => Fix_Format (fun rec => SpecsDSL_denote (F rec)) *)
   end.
