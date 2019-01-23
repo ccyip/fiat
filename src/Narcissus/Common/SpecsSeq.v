7411a0b69870f3774b3059fb3b863db2ad1b80a7
Qianchuan Ye <yeqianchuan@gmail.com>
2019-01-23 14:23:13 -0500

    Added relation between unannotated formats and format sequences.

--- src/Narcissus/Common/SpecsSeq.v
+++ src/Narcissus/Common/SpecsSeq.v
@@ -20,6 +20,14 @@ Section Specification_Sequence.
     | SSeq_cons _ _ _ seq' => Datatypes.S (SpecsSeq_len seq')
     end.
 
+  Fixpoint SpecsSeq_append {bf bm bt : bool}
+           (seq1 : SpecsSeq bf bm) (seq2 : SpecsSeq bm bt)
+    : SpecsSeq bf bt :=
+    match seq1 with
+    | SSeq_one dsl b => SSeq_cons bf dsl b seq2
+    | SSeq_cons _ dsl b seq' => SSeq_cons bf dsl b (SpecsSeq_append seq' seq2)
+    end.
+
   (* Definition SpecsSeq_lift {n} *)
   (*   : Vector.t (SpecsDSL S T) (Datatypes.S n) -> SpecsSeq false false := *)
   (*   Vector.rectS (fun _ _ => SpecsSeq false false) *)
@@ -32,5 +40,15 @@ Section Specification_Sequence.
   (*   | SSeq_one fmt _ => Vector.cons _ fmt _ (Vector.nil _) *)
   (*   | SSeq_cons _ fmt _ seq' => Vector.cons _ fmt _ (SpecsSeq_erase seq') *)
   (*   end. *)
+
+  Inductive SpecsDSL_Seq_Sim : SpecsDSL S T -> SpecsSeq false false -> Prop :=
+  | SSS_Atomic: forall dsl,
+      SpecsDSL_Atomic dsl ->
+      SpecsDSL_Seq_Sim dsl (SSeq_one false false dsl false)
+  | SSS_Sequence: forall dsl1 seq1 dsl2 seq2,
+      SpecsDSL_Seq_Sim dsl1 seq1 ->
+      SpecsDSL_Seq_Sim dsl2 seq2 ->
+      SpecsDSL_Seq_Sim (SL_Sequence dsl1 dsl2) (SpecsSeq_append seq1 seq2)
+  .
 
 End Specification_Sequence.
