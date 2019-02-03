c01eb3b2422f92b67e6b0c6f3484c2dc577d00a9
Qianchuan Ye <yeqianchuan@gmail.com>
2019-02-03 18:28:03 -0500

    Added rules for annotated formats.

--- src/Narcissus/Common/SpecsRules.v
+++ src/Narcissus/Common/SpecsRules.v
@@ -34,23 +34,31 @@ Definition compose_constr {S A gamma} (fmt : FormatDSL S A) (C : ConstrT gamma S
     (fun C =>
        fun a => exists s, (FormatDSL_denote fmt) s ∋ a /\ C s) C.
 
+Definition Union_Decode' {S T} P0 dec1 dec2 := @Union_Decode S T dec1 dec2 P0.
+
+Definition Compose_Decode' {S T S'} (dec1 : DecodeM (S'*T) T) (dec2 : DecodeM S S')
+  : DecodeM (S*T) T :=
+  fun t => st <- dec1 t; a <- dec2 (fst st); Some (a, (snd st)).
+
+Definition Lift_Decode {S T} `{Monoid T} (dec : DecodeM S T) : DecodeM (S*T) T :=
+  fun t => s <- dec t; Some (s, mempty).
 
 Inductive FormatDSL_CorrectDecoder :
   forall {S T A} (gamma : GammaT), ConstrT gamma S -> FormatDSL S A ->
                     FormatDSL S T -> DecT gamma A T -> Prop :=
+
 | CD_Union: forall S T A gamma C fmt1 fmt2 dec1 dec2
               (R : FormatDSL S A)
               (P0 : T -> bool),
     FormatDSL_CorrectDecoder gamma C R fmt1 dec1 ->
     FormatDSL_CorrectDecoder gamma C R fmt2 dec2 ->
-    (forall s t, FormatDSL_denote (FL_Union fmt1 fmt2) s ∋ t ->
-            if P0 t then FormatDSL_denote fmt1 s ∋ t
-            else FormatDSL_denote fmt2 s ∋ t) ->
+    (forall s t,
+        FormatDSL_denote (FL_Union fmt1 fmt2) s ∋ t ->
+        if P0 t then FormatDSL_denote fmt1 s ∋ t
+        else FormatDSL_denote fmt2 s ∋ t) ->
     FormatDSL_CorrectDecoder
       gamma C R (FL_Union fmt1 fmt2)
-      (GammaF_fmap2
-         (fun dec1 dec2 => Union_Decode dec1 dec2 P0)
-         dec1 dec2)
+      (GammaF_fmap2 (Union_Decode' P0) dec1 dec2)
 
 | CD_Compose1: forall S A T A' gamma C
                  (fmt1 : FormatDSL A T) (fmt2 : FormatDSL S A)
@@ -75,8 +83,76 @@ Inductive FormatDSL_CorrectDecoder :
 
 with AFormatDSL_CorrectDecoder :
        forall {S T A} (gamma : GammaT), ConstrT gamma S -> FormatDSL S A ->
-                         AFormatDSL S T -> DecT gamma (S*T) T -> Prop :=
+                         AFormatDSL S T -> DecT gamma (A*T) T -> Prop :=
 
+| CDN_Lift: forall S T `{Monoid T} A gamma C fmt dec (R : FormatDSL S A),
+    FormatDSL_CorrectDecoder gamma C R fmt dec ->
+    AFormatDSL_CorrectDecoder gamma C R (AFL_None fmt)
+                              (GammaF_fmap Lift_Decode dec)
+
+| CDR_Union: forall S T `{Monoid T} A gamma C fmt1 fmt2 dec1 dec2
+               (R : FormatDSL S A)
+               (P0 : T -> bool),
+    AFormatDSL_CorrectDecoder gamma C R (AFL_Right fmt1) dec1 ->
+    AFormatDSL_CorrectDecoder gamma C R (AFL_Right fmt2) dec2 ->
+    (forall s t t',
+        FormatDSL_denote (FL_Union fmt1 fmt2) s ∋ t ->
+        if P0 (mappend t t') then FormatDSL_denote fmt1 s ∋ t
+        else FormatDSL_denote fmt2 s ∋ t) ->
+    AFormatDSL_CorrectDecoder
+      gamma C R (AFL_Right (FL_Union fmt1 fmt2))
+      (GammaF_fmap2 (Union_Decode' P0) dec1 dec2)
+
+| CDL_Union: forall S T `{Monoid T} A gamma C fmt1 fmt2 dec1 dec2
+               (R : FormatDSL S A)
+               (P0 : T -> bool),
+    AFormatDSL_CorrectDecoder gamma C R (AFL_Left fmt1) dec1 ->
+    AFormatDSL_CorrectDecoder gamma C R (AFL_Left fmt2) dec2 ->
+    (forall s t t',
+        FormatDSL_denote (FL_Union fmt1 fmt2) s ∋ t ->
+        if P0 (mappend t' t) then FormatDSL_denote fmt1 s ∋ t
+        else FormatDSL_denote fmt2 s ∋ t) ->
+    AFormatDSL_CorrectDecoder
+      gamma C R (AFL_Left (FL_Union fmt1 fmt2))
+      (GammaF_fmap2 (Union_Decode' P0) dec1 dec2)
+
+| CDR_Compose1: forall S A T A' gamma C
+                  (fmt1 : FormatDSL A T) (fmt2 : FormatDSL S A)
+                  dec1 dec2
+                  (R : FormatDSL S A'),
+    AFormatDSL_CorrectDecoder gamma (compose_constr fmt2 C)
+                             (FL_Arbitrary IdentityFormat)
+                             (AFL_Right fmt1) dec1 ->
+    FormatDSL_CorrectDecoder gamma C R fmt2 dec2 ->
+    AFormatDSL_CorrectDecoder gamma C R (AFL_Right (FL_Compose fmt1 fmt2))
+                              (GammaF_fmap2 Compose_Decode' dec1 dec2)
+
+| CDL_Compose1: forall S A T A' gamma C
+                  (fmt1 : FormatDSL A T) (fmt2 : FormatDSL S A)
+                  dec1 dec2
+                  (R : FormatDSL S A'),
+    AFormatDSL_CorrectDecoder gamma (compose_constr fmt2 C)
+                             (FL_Arbitrary IdentityFormat)
+                             (AFL_Left fmt1) dec1 ->
+    FormatDSL_CorrectDecoder gamma C R fmt2 dec2 ->
+    AFormatDSL_CorrectDecoder gamma C R (AFL_Left (FL_Compose fmt1 fmt2))
+                              (GammaF_fmap2 Compose_Decode' dec1 dec2)
+
+| CDR_Compose2: forall S A T A' gamma C
+                  (fmt1 : FormatDSL A T) (fmt2 : FormatDSL S A)
+                  dec1 (R : FormatDSL A A'),
+    AFormatDSL_CorrectDecoder gamma (compose_constr fmt2 C)
+                             R (AFL_Right fmt1) dec1 ->
+    ~(R ~= FL_Arbitrary (@IdentityFormat A)) ->
+    AFormatDSL_CorrectDecoder gamma C (FL_Compose R fmt2) (AFL_Right (FL_Compose fmt1 fmt2)) dec1
+
+| CDL_Compose2: forall S A T A' gamma C
+                  (fmt1 : FormatDSL A T) (fmt2 : FormatDSL S A)
+                  dec1 (R : FormatDSL A A'),
+    AFormatDSL_CorrectDecoder gamma (compose_constr fmt2 C)
+                             R (AFL_Left fmt1) dec1 ->
+    ~(R ~= FL_Arbitrary (@IdentityFormat A)) ->
+    AFormatDSL_CorrectDecoder gamma C (FL_Compose R fmt2) (AFL_Left (FL_Compose fmt1 fmt2)) dec1
 
 with FormatSeq_CorrectDecoder :
        forall {S T A} (gamma : GammaT), ConstrT gamma S -> FormatDSL S A ->
